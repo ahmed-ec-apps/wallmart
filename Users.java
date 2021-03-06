69491e7644ee97e7524c93b0cf1a94cf876971ff
Ahemad Sayyed Mahemood <ahemadsayyed11@gmail.com>
2021-03-06 22:42:00 +0530

    Users file updated

--- Users.java
+++ Users.java
@@ -2,6 +2,12 @@ class testdev
 {
 	public static void main(String[] args)
 	{
+		testdev td = new testdev();
+		td.show();
 		System.out.print("Hello DevOps");
 	}
+	void show()
+	{
+		System.out.println("Show what you have");
+	}	
 }
